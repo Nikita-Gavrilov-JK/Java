d3fb183cb1c6c288293f3c65b9a07af8fd57862e
Nikita Gavrilov <n.gavrilov1999@mail.ru>
2022-05-29 00:17:27 +0400

    Добавили 3 класса: Field, Figure и Tetris.

---
+++ Tetris/src/com/example/Tetris.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Tetris {
+
+    private Field field;
+    private Figure figure;
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println();
+    }
+}
