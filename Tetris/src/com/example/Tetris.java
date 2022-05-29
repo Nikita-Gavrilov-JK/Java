4f01656a92072d1ad8632e073467d93cef339b69
Nikita Gavrilov <n.gavrilov1999@mail.ru>
2022-05-29 16:28:04 +0400

    Добавил 2 гетера.

--- Tetris/src/com/example/Tetris.java
+++ Tetris/src/com/example/Tetris.java
@@ -5,8 +5,18 @@ public class Tetris {
     private Field field;
     private Figure figure;
 
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Jojo");
+
+
     }
 }
