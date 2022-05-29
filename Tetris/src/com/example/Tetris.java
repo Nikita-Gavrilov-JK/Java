315f2518fa17315d095b3ad055d39eb3d5a5fb97
Nikita Gavrilov <n.gavrilov1999@mail.ru>
2022-05-29 16:33:20 +0400

    Добавил 2 метода -> run and step.

--- Tetris/src/com/example/Tetris.java
+++ Tetris/src/com/example/Tetris.java
@@ -13,10 +13,16 @@ public class Tetris {
         return figure;
     }
 
+    public void run() {
+
+    }
+
+    public void step() {
+
+    }
+
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Jojo");
-
-
     }
 }
